d2c6d785258b3e9216b66cc17cd719558a322e5b
Murad Sarker <sarker24@gmail.com>
2021-05-23 11:54:27 +0200

    Add the top-banner and fix the header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import TopBanner from "./components/TopBanner/TopBanner";
+
+
+
+function App() {
+  return (
+    <div>
+      <TopBanner />
+      </div>
+  );
+}
+
+export default App;
