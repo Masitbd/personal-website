f332c8ad9a54290533559cb52ef874d5632a0669
Murad Sarker <sarker24@gmail.com>
2021-05-24 02:28:38 +0200

    Add client number

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -9,10 +9,21 @@ class Summary extends Component {
                     <div className="summaryBannerOverlay">
                         <Container>
                             <Row>
-                                <Col>
-
+                                <Col lg={8} md={6} sm={12} >
+                                  <Row>
+                                      <Col className="text-center">
+                                         <h1>100</h1>
+                                          <h4>Total Projects</h4>
+                                          <hr className="bg-white w-25" />
+                                      </Col>
+                                      <Col className="text-center">
+                                          <h1>100</h1>
+                                          <h4>Total Projects</h4>
+                                          <hr className="bg-white w-25" />
+                                      </Col>
+                                  </Row>
                                 </Col>
-                                <Col>
+                                <Col lg={4} md={6} md={12}>
 
                                 </Col>
                                 <Col>
