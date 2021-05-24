d5927dd86d6d6facd837f4214ce78bd0942edb02
Murad Sarker <sarker24@gmail.com>
2021-05-24 11:06:28 +0200

    Add  card in summary section

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,5 +1,5 @@
 import React, {Component, Fragment} from 'react';
-import {Button, Col, Container, Row} from "react-bootstrap";
+import {Button, Col, Container, Row, Card} from "react-bootstrap";
 
 class Summary extends Component {
     render() {
@@ -10,25 +10,33 @@ class Summary extends Component {
                         <Container>
                             <Row>
                                 <Col lg={8} md={6} sm={12} >
-                                  <Row>
+                                  <Row className="countSections">
                                       <Col className="text-center">
-                                         <h1>100</h1>
-                                          <h4>Total Projects</h4>
+                                         <h1 className="countNumber ">100</h1>
+                                          <h4 className="countTitles">Total Projects</h4>
                                           <hr className="bg-white w-25" />
                                       </Col>
                                       <Col className="text-center">
-                                          <h1>100</h1>
-                                          <h4>Total Projects</h4>
+                                          <h1 className="countNumber">100</h1>
+                                          <h4 className="countTitles">Total Clients</h4>
                                           <hr className="bg-white w-25" />
                                       </Col>
                                   </Row>
                                 </Col>
                                 <Col lg={4} md={6} md={12}>
-
+                                    <Card style={{ width: '18rem' }}>
+                                        <Card.Body>
+                                            <Card.Title className="cardTitle">How I Work</Card.Title>
+                                            <Card.Text>
+                                               <p className="cardSubTitle">Requirement Analysis</p>
+                                                <p className="cardSubTitle">System Analysis</p>
+                                                <p className="cardSubTitle">Software Testing</p>
+                                                <p className="cardSubTitle">Implementation</p>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
                                 </Col>
-                                <Col>
 
-                                </Col>
                             </Row>
                         </Container>
                     </div>
