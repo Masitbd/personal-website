f9c1463df9cf0125dccf607c97029477017f1927
Murad Sarker <sarker24@gmail.com>
2021-05-25 00:27:59 +0200

    Slider setting

---
+++ src/components/ClientReview/ClientReview.js
@@ -0,0 +1,53 @@
+import React, {Component, Fragment} from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+class ClientReview extends Component {
+    render() {
+        var settings = {
+            autoPlaySpeed: 3000,
+            autoplay: true,
+            dots: true,
+            infinite: true,
+            speed: 3000,
+            vertical: true,
+            verticalSwiping: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        initialSlide: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        };
+        return (
+            <Fragment>
+
+            </Fragment>
+        );
+    }
+}
+
+export default ClientReview;
