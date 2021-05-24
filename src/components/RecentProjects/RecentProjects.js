01ddcceeb622490b10c6ae7d393427e0b3c55206
Murad Sarker <sarker24@gmail.com>
2021-05-24 16:42:01 +0200

    completed recent project section

--- src/components/RecentProjects/RecentProjects.js
+++ src/components/RecentProjects/RecentProjects.js
@@ -9,41 +9,41 @@ class RecentProjects extends Component {
                     <h1 className="serviceNameTitles text-center">Recent Projects</h1>
                     <Row>
                         <Col lg={4} md={6} sm={12}>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2018/04/19/20/44/systems-icons-3334262_960_720.jpg" />
+                            <Card className="projectCard">
+                                <Card.Img className="projectCardImage" variant="top" src="https://cdn.pixabay.com/photo/2018/04/19/20/44/systems-icons-3334262_960_720.jpg" />
                                 <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
+                                    <Card.Title className="projectCardTitle">Personal Website</Card.Title>
+                                    <Card.Text className="projectCardDesc">
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
                                     </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Details</Button>
                                 </Card.Body>
                             </Card>
                         </Col>
                         <Col lg={4} md={6} sm={12}>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2018/04/19/20/44/systems-icons-3334262_960_720.jpg" />
+                            <Card className="projectCard">
+                                <Card.Img className="projectCardImage" variant="top" src="https://cdn.pixabay.com/photo/2016/11/18/14/05/brick-wall-1834784_960_720.jpg" />
                                 <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
+                                    <Card.Title className="projectCardTitle">Restaurant Billing system</Card.Title>
+                                    <Card.Text className="projectCardDesc">
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
                                     </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Details</Button>
                                 </Card.Body>
                             </Card>
                         </Col>
                         <Col lg={4} md={6} sm={12}>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2018/04/19/20/44/systems-icons-3334262_960_720.jpg" />
+                            <Card className="projectCard">
+                                <Card.Img className="projectCardImage" variant="top" src="https://cdn.pixabay.com/photo/2016/05/21/21/52/house-1407562_960_720.jpg" />
                                 <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
+                                    <Card.Title className="projectCardTitle">House Rent Management System</Card.Title>
+                                    <Card.Text className="projectCardDesc">
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
                                     </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Details</Button>
                                 </Card.Body>
                             </Card>
                         </Col>
