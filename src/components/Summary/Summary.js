82d9f2fbe2a998777bda45a81b70d328b50afbe4
Murad Sarker <sarker24@gmail.com>
2021-05-24 14:46:19 +0200

    Count animation setup

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,5 +1,9 @@
 import React, {Component, Fragment} from 'react';
 import {Button, Col, Container, Row, Card} from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import CountUp from 'react-countup';
+import VisibilitySensor from "react-visibility-sensor";
 
 class Summary extends Component {
     render() {
@@ -12,26 +16,42 @@ class Summary extends Component {
                                 <Col lg={8} md={6} sm={12} >
                                   <Row className="countSections">
                                       <Col className="text-center">
-                                         <h1 className="countNumber ">100</h1>
+                                         <h1 className="countNumber ">
+                                             <CountUp start={0} end={100}>
+                                                 {({ countUpRef, start }) => (
+                                                     <VisibilitySensor onChange={start} delayedCall>
+                                                             <span ref={countUpRef} />
+                                                         </VisibilitySensor>
+                                                     )}
+                                             </CountUp>
+                                         </h1>
                                           <h4 className="countTitles">Total Projects</h4>
                                           <hr className="bg-white w-25" />
                                       </Col>
                                       <Col className="text-center">
-                                          <h1 className="countNumber">100</h1>
+                                          <h1 className="countNumber">
+                                              <CountUp start={0} end={100}>
+                                                  {({ countUpRef, start }) => (
+                                                      <VisibilitySensor onChange={start} delayedCall>
+                                                          <span ref={countUpRef} />
+                                                      </VisibilitySensor>
+                                                  )}
+                                              </CountUp>
+                                          </h1>
                                           <h4 className="countTitles">Total Clients</h4>
                                           <hr className="bg-white w-25" />
                                       </Col>
                                   </Row>
                                 </Col>
                                 <Col lg={4} md={6} md={12}>
-                                    <Card style={{ width: '18rem' }}>
+                                    <Card className="workCard">
                                         <Card.Body>
                                             <Card.Title className="cardTitle">How I Work</Card.Title>
                                             <Card.Text>
-                                               <p className="cardSubTitle">Requirement Analysis</p>
-                                                <p className="cardSubTitle">System Analysis</p>
-                                                <p className="cardSubTitle">Software Testing</p>
-                                                <p className="cardSubTitle">Implementation</p>
+                                               <p className="cardSubTitle"><FontAwesomeIcon className="iconBullet" icon={faCheckCircle} />Requirement Analysis</p>
+                                                <p className="cardSubTitle"><FontAwesomeIcon className="iconBullet" icon={faCheckCircle} />System Analysis</p>
+                                                <p className="cardSubTitle"><FontAwesomeIcon className="iconBullet" icon={faCheckCircle} />Software Testing</p>
+                                                <p className="cardSubTitle"><FontAwesomeIcon className="iconBullet" icon={faCheckCircle} />Implementation</p>
                                             </Card.Text>
                                         </Card.Body>
                                     </Card>
